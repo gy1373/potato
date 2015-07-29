6cc998b862f5e975864bd6f7b8d1e3463402b020
Justin Miron <justinmiron11@gmail.com>
2015-07-29 13:42:14 -0700

    restructure folder

---
+++ eventapp/app/src/main/java/com/example/blaisdell2/fragments/MapFragment.java
@@ -0,0 +1,54 @@
+package com.example.blaisdell2.fragments;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.justin.eventapp.R;
+
+/**
+ * Created by justin on 7/28/15.
+ */
+public class MapFragment extends Fragment {
+
+    public static MapFragment newInstance() {
+        MapFragment fragment = new MapFragment();
+        return fragment;
+    }
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
